ddb81f8adb2bc12a632875be4685236cfb833b47
trinhlk2 <emperorfire52@gmail.com>
2023-11-05 10:43:10 +0700

    Add thêm thông tin cột vào output cuối

--- scripts/api/out_data_api.py
+++ scripts/api/out_data_api.py
@@ -32,24 +32,24 @@ def out_data_api():
 
     print('2. Tính toán quận huyện quá tải')
     qua_tai1 = ngung_giao_nhan.loc[ngung_giao_nhan['score'] == -10][
-        ['receiver_province', 'receiver_district', 'nvc']]
-    qua_tai1['status'] = 'Quá tải (Ngưng giao nhận)'
+        ['receiver_province', 'receiver_district', 'carrier']]
+    qua_tai1['carrier_status'] = 'Quá tải (Ngưng giao nhận)'
 
-    qua_tai2 = danh_gia_zns.loc[danh_gia_zns['score'] == -10][['receiver_province', 'receiver_district', 'nvc']]
-    qua_tai2['status'] = 'Quá tải (Đánh giá ZNS)'
+    qua_tai2 = danh_gia_zns.loc[danh_gia_zns['score'] == -10][['receiver_province', 'receiver_district', 'carrier']]
+    qua_tai2['carrier_status'] = 'Quá tải (Đánh giá ZNS)'
 
-    qua_tai3 = ti_le_giao_hang.loc[ti_le_giao_hang['score'] == -10][['receiver_province', 'receiver_district', 'nvc']]
-    qua_tai3['status'] = 'Quá tải (Tỉ lệ giao hàng)'
+    qua_tai3 = ti_le_giao_hang.loc[ti_le_giao_hang['score'] == -10][['receiver_province', 'receiver_district', 'carrier']]
+    qua_tai3['carrier_status'] = 'Quá tải (Tỉ lệ giao hàng)'
 
     qua_tai4 = thoi_gian_giao_hang.loc[thoi_gian_giao_hang['score'] == -10][
-        ['receiver_province', 'receiver_district', 'nvc']]
-    qua_tai4['status'] = 'Quá tải (Thời gian giao hàng)'
+        ['receiver_province', 'receiver_district', 'carrier']]
+    qua_tai4['carrier_status'] = 'Quá tải (Thời gian giao hàng)'
 
-    qua_tai5 = kho_giao_nhan.loc[kho_giao_nhan['score'] == -10][['receiver_province', 'receiver_district', 'nvc']]
-    qua_tai5['status'] = 'Quá tải (Kho giao nhận)'
+    qua_tai5 = kho_giao_nhan.loc[kho_giao_nhan['score'] == -10][['receiver_province', 'receiver_district', 'carrier']]
+    qua_tai5['carrier_status'] = 'Quá tải (Kho giao nhận)'
 
     qua_tai = pd.concat([qua_tai1, qua_tai2, qua_tai3, qua_tai4, qua_tai5]).drop_duplicates(
-        ['receiver_province', 'receiver_district', 'nvc'])
+        ['receiver_province', 'receiver_district', 'carrier'])
 
     print('3. Xử lý data thời gian giao dịch')
     thoi_gian_giao_hang = thoi_gian_giao_hang.rename(columns={'delivery_time_mean_h': 'estimate_delivery_time_details'})
@@ -59,8 +59,8 @@ def out_data_api():
         round_value)
 
     # Thời gian giao hàng default
-    nvc_df = pd.DataFrame(data={
-        'nvc': list(MAPPING_ID_NVC.values()),
+    active_carrier_df = pd.DataFrame(data={
+        'carrier': ACTIVE_CARRIER,
     })
     loai_van_chuyen_df = pd.DataFrame(THOI_GIAN_GIAO_HANG_DEFAULT.items(),
                                       columns=['order_type', 'default_delivery_time'])
@@ -71,8 +71,8 @@ def out_data_api():
             'province': 'receiver_province',
             'district': 'receiver_district',
         })
-            .merge(
-            nvc_df.merge(loai_van_chuyen_df, how='cross'),
+        .merge(
+            active_carrier_df.merge(loai_van_chuyen_df, how='cross'),
             how='cross'
         )
     )
@@ -80,8 +80,8 @@ def out_data_api():
     thoi_gian_giao_hang_final = (
         thoi_gian_giao_hang_default.merge(
             thoi_gian_giao_hang,
-            on=['receiver_province', 'receiver_district', 'nvc', 'order_type'],
-            how='inner'
+            on=['receiver_province', 'receiver_district', 'carrier', 'order_type'],
+            how='left'
         )
     )
 
@@ -99,13 +99,13 @@ def out_data_api():
     score_df_list = []
 
     for target_df in [danh_gia_zns, ti_le_giao_hang, chat_luong_noi_bo, thoi_gian_giao_hang, kho_giao_nhan]:
-        target_df['score_trong_so'] = target_df['score'] * target_df['trong_so']
-        score_df_list.append(target_df[['receiver_province', 'receiver_district', 'nvc', 'score_trong_so']])
+        target_df['weight_score'] = target_df['score'] * target_df['criteria_weight']
+        score_df_list.append(target_df[['receiver_province', 'receiver_district', 'carrier', 'weight_score']])
 
     score_df = pd.concat(score_df_list, ignore_index=False)
-    score_final = score_df.groupby(['receiver_province', 'receiver_district', 'nvc'])[
-        'score_trong_so'].mean().reset_index()
-    score_final = score_final.rename(columns={'score_trong_so': 'score'})
+    score_final = score_df.groupby(['receiver_province', 'receiver_district', 'carrier'])[
+        'weight_score'].mean().reset_index()
+    score_final = score_final.rename(columns={'weight_score': 'score'})
 
     q_lower = score_final['score'].quantile(0.005)
     q_upper = score_final['score'].quantile(0.995)
@@ -124,29 +124,39 @@ def out_data_api():
     # 6. Combine api data
     print('6. Combine api data')
     api_data_final = (
-        ngung_giao_nhan[[
-            'receiver_province', 'receiver_district', 'nvc',
-        ]].merge(
-            thoi_gian_giao_hang_final[[
-                'receiver_province', 'receiver_district', 'nvc', 'order_type',
-                'estimate_delivery_time', 'estimate_delivery_time_details',
-            ]], on=['receiver_province', 'receiver_district', 'nvc'],
-            how='inner'
-        ).merge(score_final, on=['receiver_province', 'receiver_district', 'nvc'], how='inner')
-            .merge(
+        thoi_gian_giao_hang_final[[
+            'receiver_province', 'receiver_district', 'carrier', 'order_type',
+            'estimate_delivery_time', 'estimate_delivery_time_details',
+        ]].merge(score_final, on=['receiver_province', 'receiver_district', 'carrier'], how='inner')
+        .merge(
             ti_le_giao_hang[[
-                'receiver_province', 'receiver_district', 'nvc', 'pct_success'
-            ]].rename(columns={'pct_success': 'proportion_deli'}),
-            on=['receiver_province', 'receiver_district', 'nvc'],
+                'receiver_province', 'receiver_district', 'carrier', 'delivery_success_rate'
+            ]], on=['receiver_province', 'receiver_district', 'carrier'],
             how='inner'
         )
     )
-    api_data_final['proportion_deli'] = np.round(api_data_final['proportion_deli'] * 100, 2)
+    api_data_final['delivery_success_rate'] = np.round(api_data_final['delivery_success_rate'] * 100, 2)
 
     print('7. Gắn thông tin quá tải')
-    api_data_final = api_data_final.merge(qua_tai, on=['receiver_province', 'receiver_district', 'nvc'], how='left')
-    api_data_final['status'] = api_data_final['status'].fillna('Bình thường')
+    api_data_final = api_data_final.merge(qua_tai, on=['receiver_province', 'receiver_district', 'carrier'], how='left')
+    api_data_final['carrier_status'] = api_data_final['carrier_status'].fillna('Bình thường')
 
+    api_data_final['carrier_id'] = api_data_final['carrier'].map(MAPPING_CARRIER_ID)
+    api_data_final = (
+        api_data_final.merge(
+            PROVINCE_MAPPING_DISTRICT_DF.rename(columns={
+                'province': 'receiver_province',
+                'district': 'receiver_district',
+                'province_id': 'receiver_province_id',
+                'district_id': 'receiver_district_id',
+            }), on=['receiver_province', 'receiver_district'], how='left'
+        )
+    )
+    api_data_final = api_data_final[[
+        'receiver_province_id', 'receiver_province', 'receiver_district_id', 'receiver_district',
+        'carrier_id', 'carrier', 'order_type', 'carrier_status', 'estimate_delivery_time_details',
+        'estimate_delivery_time', 'delivery_success_rate', 'score', 'stars',
+    ]]
     print('8. Lưu dữ liệu API')
     with open('./output/data_api.json', 'w', encoding='utf-8') as file:
         api_data_final.to_json(file, force_ascii=False)
@@ -157,27 +167,32 @@ def out_data_api():
     print('-' * 100)
 
 
-def combine_data_api_with_tien_giao_dich():
-    print('1. Đọc thông tin data API + tiền giao dịch')
+def combine_full_data():
+    # Lấy toàn bộ data output API join với data giao dịch có khối lượng
+    # để tunning score, check lỗi
+
+    print('1. Đọc thông tin data API + giao dịch có khối lượng')
     api_data_final = pd.read_parquet('./output/data_api.parquet')
     _, _, _, _, _, _, tien_giao_dich = total_transform()
 
     full_information_df = (
         tien_giao_dich[[
             'order_id', 'receiver_province', 'receiver_district',
-            'nvc', 'weight', 'order_type', 'service_fee'
+            'carrier', 'order_type', 'weight', 'service_fee'
         ]].merge(
             api_data_final,
-            on=['receiver_province', 'receiver_district', 'nvc', 'order_type'],
+            on=['receiver_province', 'receiver_district', 'carrier', 'order_type'],
             how='left'
         )
     )
-    full_information_df['id_nvc'] = full_information_df['nvc'].map(MAPPING_NVC_ID)
-    assert len(full_information_df) == len(tien_giao_dich), 'Transform data API không chính xác'
+    full_information_df['carrier_id'] = full_information_df['carrier'].map(MAPPING_CARRIER_ID)
+    assert len(full_information_df) == len(tien_giao_dich), 'Transform data không chính xác'
 
     print('2. Tính toán notification')
+    # Transform này chỉ lấy được 1 dòng service_fee nhỏ nhất
     re_nhat_df = full_information_df.groupby(['order_id'])['service_fee'].min().reset_index()
     re_nhat_df['notification'] = 'Rẻ nhất'
+    # Gắn ngược lại để lấy đủ row (trong trường hợp có nhiều carrier cùng mức giá
     re_nhat_df = re_nhat_df.merge(full_information_df, on=['order_id', 'service_fee'], how='inner')
 
     full_information_df1 = merge_left_only(full_information_df, re_nhat_df, keys=['order_id', 'service_fee'])
@@ -205,9 +220,11 @@ def combine_data_api_with_tien_giao_dich():
     ], ignore_index=True)
 
     full_information_df = full_information_df[[
-        'order_id', 'id_nvc', 'nvc', 'receiver_province', 'receiver_district',
-        'order_type', 'status', 'weight', 'service_fee', 'proportion_deli',
-        'estimate_delivery_time', 'estimate_delivery_time_details',
+        'order_id',
+        'receiver_province_id', 'receiver_province', 'receiver_district_id', 'receiver_district',
+        'carrier_id', 'carrier', 'order_type', 'weight', 'service_fee',
+        'carrier_status',
+        'estimate_delivery_time_details', 'estimate_delivery_time', 'delivery_success_rate',
         'score', 'stars', 'notification',
     ]]
 
@@ -220,4 +237,4 @@ def combine_data_api_with_tien_giao_dich():
 
 if __name__ == '__main__':
     out_data_api()
-    combine_data_api_with_tien_giao_dich()
+    combine_full_data()
