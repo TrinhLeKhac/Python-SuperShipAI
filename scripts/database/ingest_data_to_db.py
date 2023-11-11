181e8961ebb9c9840b60255a3897c0268ebb8437
trinhlk2 <emperorfire52@gmail.com>
2023-11-12 00:29:59 +0700

    Add files ingest data to DB

--- scripts/database/ingest_data_to_db.py
+++ scripts/database/ingest_data_to_db.py
@@ -1,29 +1,30 @@
 from sqlalchemy import create_engine
-from scripts.utilities.helper import *
-from scripts.api.out_data_api import *
 from scripts.api.out_data_final import *
+from scripts.database.dtypes import *
 
 
 def ingest_data_to_db():
-    engine = create_engine('postgresql://postgres:123456@localhost:5432/supership_ai_db')
+    port = create_engine('postgresql://postgres:123456@localhost:5432/supership_ai_db')
+    engine = create_engine(port)
 
     print('1. Ingest processed_data...')
     processed_data_path = './processed_data'
-    for f in os.listdir(processed_data_path):
-        tmp_df = pd.read_parquet(os.path.join(processed_data_path, f))
-        tmp_df.to_sql(file, engine)
+    for f, schema in TABLE_SCHEMA.values():
+        if f not in ['data_api', 'data_api_full', 'data_check_output']:
+            tmp_df = pd.read_parquet(os.path.join(processed_data_path, f))
+            tmp_df.to_sql(name=file, con=engine, schema="db_schema", index=False, dtype=schema)
 
     print('2. Ingest output API')
     data_api_df = pd.read_parquet('./output/data_api.parquet')
-    data_api_df.to_sql("data_api", engine)
+    data_api_df.to_sql(name='data_api', con=engine, schema="db_schema", index=False, dtype=TABLE_SCHEMA['data_api'])
 
     print('3. Ingest output API full')
     data_api_full_df = out_data_api(return_full_cols_df=True)
-    data_api_full_df.to_sql("data_api_full", engine)
+    data_api_full_df.to_sql(name='data_api_full', con=engine, schema="db_schema", index=False, dtype=TABLE_SCHEMA['data_api_full'])
 
     print('4. Ingest data check output')
     final_df = out_data_final()
-    data_api_full_df.to_sql('data_check_output', engine)
+    data_api_full_df.to_sql(name='data_check_output', con=engine, schema="db_schema", index=False, dtype=TABLE_SCHEMA['data_check_output'])
 
 
 if __name__ == '__main__':
